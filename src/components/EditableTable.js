dda8cd665e0d50f5a771b7d57443a113dd7e4cd3
Alexander Patola <alexanderpatola@gmail.com>
2022-07-21 21:25:51 +0200

    Rows and Columns can now be deleted

--- src/components/EditableTable.js
+++ src/components/EditableTable.js
@@ -4,10 +4,7 @@ import IconButton from "./IconButton";
 import React from "react";
 import "./EditableTable.css";
 
-const EditableTable = React.forwardRef((props, ref) => {
-
-    const [selectedRow,setSelectedRow] = useState(0)
-    const [selectedColumn, setSelectedColumn] = useState(0)
+const EditableTable = React.forwardRef(({removeTable}, ref) => {
 
     const [tableRows, setTableRows] = useState([
         {
@@ -17,14 +14,32 @@ const EditableTable = React.forwardRef((props, ref) => {
                     id:uuid(),
                     className:"table-cell",
                     content:null
+                },
+                {
+                    id:uuid(),
+                    className:"table-cell",
+                    content:null
+                }
+            ]
+        },
+        {
+            id:uuid(),
+            columns:[
+                {
+                    id:uuid(),
+                    className:"table-cell",
+                    content:null
+                },
+                {
+                    id:uuid(),
+                    className:"table-cell",
+                    content:null
                 }
             ]
         }
     ])
     const [columnCount, setColumnCount] = useState(1)
-
-    const [tableContent, setTableContent] = useState()
-    const [selectedCell, setSelectedCell] = useState(null)
+    let selectedCell = {row:null,col:null}
 
     const bodyRef = useRef()
 
@@ -36,15 +51,14 @@ const EditableTable = React.forwardRef((props, ref) => {
             addRow()
         },
         removeColumn(){
-
+            remColumn()
         },
         removeRow(){
-
+            remRow()
         }
     }))
 
     function addRow() {
-        console.log(tableRows)
         let id = uuid()
         setTableRows(prev => [...prev,
             {
@@ -54,6 +68,17 @@ const EditableTable = React.forwardRef((props, ref) => {
         ])
     }
 
+    // Removes a Row from the Context Table
+    function remRow(){
+        let index = tableRows.findIndex(x => x.id === selectedCell.row)
+
+        const elements = [...tableRows]
+        elements.splice(index,1)
+
+        setTableRows(elements)
+    }
+
+    // Appends a row to the Context Table
     function addColumn() {
         tableRows.map(x => {
             x.columns.push(
@@ -64,10 +89,24 @@ const EditableTable = React.forwardRef((props, ref) => {
                 }
             )
         })
-        //setTableRows(tableRows);
         setColumnCount(columnCount+1);
     }
 
+    // Removes a Column from the Context Table
+    function remColumn(){
+        const rows = [...tableRows]
+
+        let index = tableRows[tableRows.findIndex(x => x.id === selectedCell.row)].columns.findIndex(x => x.id === selectedCell.col)
+        for (let i = 0; i < tableRows.length; i++) {
+            const elements = [...rows[i].columns]
+            elements.splice(index,1)
+            rows[i].columns = elements;
+        }
+        setTableRows(rows)
+        setColumnCount(columnCount-1)
+    }
+
+    // Creates a new Column for a new Row
     const createColumns = () =>{
         let columns = []
         for (let i = 0; i < columnCount; i++) {
@@ -82,6 +121,7 @@ const EditableTable = React.forwardRef((props, ref) => {
         return columns;
     }
 
+    // Saves input Data of a cell on input changed
     const oninputChanged = (params) => {
         const rowId = params.target.parentElement.id;
         const colId = params.target.id;
@@ -91,16 +131,33 @@ const EditableTable = React.forwardRef((props, ref) => {
         console.log(params)
     }
 
+    // Saves the current selected cell
+    function onCellSelected(params){
+        const rowId = params.target.parentElement.id;
+        const colId = params.target.id;
+
+        selectedCell = {row:rowId,col:colId};
+    }
+
     return(
         <div className="outer-table-container">
             <div className="options-container">
-                <input className="table-title" contentEditable={true}/>
+                <input placeholder="Enter a title..." className="table-title" contentEditable={true}/>
                 <div className="hidden-table-options">
                     <IconButton onClick={addRow}>
                         <img src="./icons/add-row-solid.svg"/>
                     </IconButton>
                     <IconButton onClick={addColumn}>
                         <img src="./icons/add-column-solid.svg"/>
+                    </IconButton>
+                    <IconButton onClick={remRow}>
+                        <img src="./icons/rem-row-solid.svg"/>
+                    </IconButton>
+                    <IconButton onClick={remColumn}>
+                        <img src="./icons/rem-column-solid.svg"/>
+                    </IconButton>
+                    <IconButton onClick={removeTable}>
+                        <img src="./icons/circle-minus-solid.svg"/>
                     </IconButton>
                 </div>
             </div>
@@ -111,7 +168,7 @@ const EditableTable = React.forwardRef((props, ref) => {
                             <tr id={x.id}>
                                 {
                                     x.columns.map(y => (
-                                        <td onInput={oninputChanged} id={y.id} className={y.className} contentEditable={true}>
+                                        <td onFocus={onCellSelected} onInput={oninputChanged} id={y.id} className={y.className} contentEditable={true}>
                                             {y.content}
                                         </td>
                                     ))
